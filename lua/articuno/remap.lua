0c2549888019f2e134d14a987b91d7ef523bb85b
Kurt Miller <kurtmilla1994@gmail.com>
2022-12-10 14:00:33 -0500

    added lsp

--- lua/articuno/remap.lua
+++ lua/articuno/remap.lua
@@ -2,3 +2,6 @@ local nnoremap = require("articuno.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("<leader>pt", "<cmd>Telescope find_files<CR>")
+nnoremap("<leader>gs", "<cmd>:Git<CR>")
+nnoremap("<leader>gb", "<cmd>:G blame<CR>")
+nnoremap("<leader>ga", "<cmd>Git fetch --all<CR>")
